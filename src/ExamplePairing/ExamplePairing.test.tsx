82105f7136487b576b730bc80b2ead7dc48bf834
Elliot Voris <elliot@stellar.org>
2024-10-22 09:59:10 -0500

    add test coverage for the curl example, and AP vs RPC URLs

--- src/ExamplePairing/ExamplePairing.test.tsx
+++ src/ExamplePairing/ExamplePairing.test.tsx
@@ -72,3 +72,174 @@ it("renders examples with params by-name", async () => {
   expect(container.innerHTML.includes("foo")).toBe(true);
   expect(container.innerHTML.includes("bar")).toBe(true);
 });
+
+it("renders example pairings as a curl example", async () => {
+  const method: MethodObject = {
+    examples: [
+      {
+        name: "fooExample",
+        params: [
+          {
+            name: "foo",
+            value: "bar",
+          },
+        ],
+        result: {
+          name: "exampleResultThing",
+          value: "potato",
+        },
+      },
+    ],
+    name: "myMethod",
+    paramStructure: "by-name",
+    params: [{
+      name: "foo",
+      schema: {
+        type: "string",
+      },
+    }],
+    result: {
+      name: "resultThing",
+      schema: {
+        type: "string",
+      },
+    },
+  };
+  const methodExamples = method.examples as MethodObjectExamples;
+  const { container } = render(
+    <ExamplePairing
+      methodName={method.name}
+      examplePairing={methodExamples[0] as ExamplePairingObject}
+      paramStructure={method.paramStructure || "by-position"} />
+  );
+  expect(container.innerHTML.includes("curl -X POST")).toBe(true);
+});
+
+// TODO: Since we are passing in `CodeBlock`, `Tabs`, and `TabItem` into the
+// example pairing component, I'm not sure how to accomplish that same thing
+// here in a test...
+
+// it("renders example pairings as a javascript example",
+// async () => { const method: MethodObject = { examples: [
+//       {
+//         name: "fooExample",
+//         params: [
+//           {
+//             name: "foo",
+//             value: "bar",
+//           },
+//         ],
+//         result: {
+//           name: "exampleResultThing",
+//           value: "potato",
+//         },
+//       },
+//     ],
+//     name: "myMethod",
+//     paramStructure: "by-name",
+//     params: [{
+//       name: "foo",
+//       schema: {
+//         type: "string",
+//       },
+//     }],
+//     result: {
+//       name: "resultThing",
+//       schema: {
+//         type: "string",
+//       },
+//     },
+//   };
+//   const methodExamples = method.examples as MethodObjectExamples;
+//   const { container } = render(
+//     <ExamplePairing
+//       methodName={method.name}
+//       examplePairing={methodExamples[0] as ExamplePairingObject}
+//       paramStructure={method.paramStructure || "by-position"} />
+//   );
+//   expect(container.innerHTML.includes("let res = await fetch")).toBe(true);
+// });
+
+it("renders example pairings with soroban-rpc testnet server", async () => {
+  const method: MethodObject = {
+    examples: [
+      {
+        name: "fooExample",
+        params: [
+          {
+            name: "foo",
+            value: "bar",
+          },
+        ],
+        result: {
+          name: "exampleResultThing",
+          value: "potato",
+        },
+      },
+    ],
+    name: "myMethod",
+    paramStructure: "by-name",
+    params: [{
+      name: "foo",
+      schema: {
+        type: "string",
+      },
+    }],
+    result: {
+      name: "resultThing",
+      schema: {
+        type: "string",
+      },
+    },
+  };
+  const methodExamples = method.examples as MethodObjectExamples;
+  const { container } = render(
+    <ExamplePairing
+      methodName={method.name}
+      examplePairing={methodExamples[0] as ExamplePairingObject}
+      paramStructure={method.paramStructure || "by-position"} />
+  );
+  expect(container.innerHTML.includes("https://soroban-testnet.stellar.org")).toBe(true);
+});
+
+it("renders example pairings with anchor platform example hostname", async () => {
+  const method: MethodObject = {
+    examples: [
+      {
+        name: "fooExample",
+        params: [
+          {
+            name: "foo",
+            value: "bar",
+          },
+        ],
+        result: {
+          name: "exampleResultThing",
+          value: "potato",
+        },
+      },
+    ],
+    name: "my_method",
+    paramStructure: "by-name",
+    params: [{
+      name: "foo",
+      schema: {
+        type: "string",
+      },
+    }],
+    result: {
+      name: "resultThing",
+      schema: {
+        type: "string",
+      },
+    },
+  };
+  const methodExamples = method.examples as MethodObjectExamples;
+  const { container } = render(
+    <ExamplePairing
+      methodName={method.name}
+      examplePairing={methodExamples[0] as ExamplePairingObject}
+      paramStructure={method.paramStructure || "by-position"} />
+  );
+  expect(container.innerHTML.includes("https://platform-server.exampleanchor.com")).toBe(true);
+});
